3aa3643d11628ae7df732ce6cf3d41c2d26c9d2f
Pedro Wiezel <131211509+pwiez@users.noreply.github.com>
2024-08-27 01:38:25 -0300

    small basic calculator exercise

---
+++ ruby/smallrubycalculator/calculator.rb
@@ -0,0 +1,30 @@
+class Calculator
+
+    def sum(*numbers)
+        return numbers.reduce(:+)
+    end
+
+    def subtract(*numbers)
+        return numbers.reduce(:-)
+    end
+
+    def multiply(*numbers)
+        return numbers.reduce(:*)
+    end
+
+    def divide(*numbers)
+        begin
+            return numbers.reduce(:/)
+        rescue ZeroDivisionError => e
+            puts "#{e}"
+        end
+    end
+
+end
+
+calc = Calculator.new
+
+puts calc.sum(1, 2, 5)
+puts calc.subtract(5, 3, 10)
+puts calc.multiply(2, 5, 10)
+puts calc.divide(100, 5, 0)
