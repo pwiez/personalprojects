5f2da9236587983b168123a275854ea2c29cc735
= <131211509+pwiez@users.noreply.github.com>
2025-01-26 22:03:00 -0300

    Exit button and code comments added

--- passwordgenerator/main.py
+++ passwordgenerator/main.py
@@ -1,11 +1,19 @@
 import tkinter as tk
 
+#configuration of the window itself (for now the geometry is fixed)
 app = tk.Tk()
 app.title("Password Generator")
 app.geometry("600x480")
 app.resizable(0, 0)
 
-intro = tk.Label(app, text="This is a very simple Python app (with a GUI! :D) to generate passwords.\nYou can choose the generated password's length and complexity.", wraplength=500, justify="center")
-intro.place(relx=0.5, rely=0.1, anchor="center")
+#configuring the window elements (more stuff will be added here)
+intro_label = tk.Label(app, text="This is a very simple Python app (with a GUI! :D) to generate passwords.\nYou can choose the generated password's length and complexity.", wraplength=500, justify="center")
+intro_label.place(relx=0.5, rely=0.1, anchor="center")
 
+#quitting the app
+exit_button = tk.Button(app, text="Exit", command=app.destroy)
+exit_button.place(relx=0.5, rely=0.9, anchor="center")
+
+#######
+#running the app
 app.mainloop()
