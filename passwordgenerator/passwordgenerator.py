318ddfe89dc3d1c749dc4924860f0294b801ad8f
Pedro Wiezel <131211509+pwiez@users.noreply.github.com>
2025-02-02 10:14:54 -0300

    Updated self.root usage

--- passwordgenerator/passwordgenerator.py
+++ passwordgenerator/passwordgenerator.py
@@ -22,7 +22,7 @@ class PasswordApp:
         self.root.resizable(0, 0)
 
         # configuring the intro label
-        self.intro_label = tk.Label(root, text="This is a very simple Python app (with a tkinter GUI :D) to generate passwords.\nYou can choose the generated password's length and complexity.\nPasswords can be from 8 to 64 characters in length.", wraplength=500, justify="center")
+        self.intro_label = tk.Label(self.root, text="This is a very simple Python app (with a tkinter GUI :D) to generate passwords.\nYou can choose the generated password's length and complexity.\nPasswords can be from 8 to 64 characters in length.", wraplength=500, justify="center")
         self.intro_label.place(relx=0.5, rely=0.1, anchor="center")
 
         # configuring the character type checkboxes
@@ -31,10 +31,10 @@ class PasswordApp:
         self.uppercase_letters_flag = tk.BooleanVar()
         self.symbols_flag = tk.BooleanVar()
 
-        self.numbers_checkbox = tk.Checkbutton(root, text="Include numbers", variable=self.numbers_flag, onvalue=True, offvalue=False)
-        self.lowercase_letters_checkbox = tk.Checkbutton(root, text="Include lowercase letters", variable=self.lowercase_letters_flag, onvalue=True, offvalue=False)
-        self.uppercase_letters_checkbox = tk.Checkbutton(root, text="Include uppercase letters", variable=self.uppercase_letters_flag, onvalue=True, offvalue=False)
-        self.symbols_checkbox = tk.Checkbutton(root, text="Include symbols", variable=self.symbols_flag, onvalue=True, offvalue=False)
+        self.numbers_checkbox = tk.Checkbutton(self.root, text="Include numbers", variable=self.numbers_flag, onvalue=True, offvalue=False)
+        self.lowercase_letters_checkbox = tk.Checkbutton(self.root, text="Include lowercase letters", variable=self.lowercase_letters_flag, onvalue=True, offvalue=False)
+        self.uppercase_letters_checkbox = tk.Checkbutton(self.root, text="Include uppercase letters", variable=self.uppercase_letters_flag, onvalue=True, offvalue=False)
+        self.symbols_checkbox = tk.Checkbutton(self.root, text="Include symbols", variable=self.symbols_flag, onvalue=True, offvalue=False)
 
         self.numbers_checkbox.place(x=55, y=107)
         self.lowercase_letters_checkbox.place(x=55, y=137)
@@ -44,26 +44,26 @@ class PasswordApp:
         # configuring the password length slider
         self.password_length = tk.IntVar()
 
-        self.password_length_label = tk.Label(root, text="Password length:")
+        self.password_length_label = tk.Label(self.root, text="Password length:")
         self.password_length_label.place(x=365, y=135)
 
-        self.password_length_scale = tk.Scale(root, length=230, from_=8, to=64, orient="horizontal", variable=self.password_length)
+        self.password_length_scale = tk.Scale(self.root, length=230, from_=8, to=64, orient="horizontal", variable=self.password_length)
         self.password_length_scale.place(relx=0.51, y=155)
 
         # configuring the button to generate the password, it calls generate_password on click
-        self.generate_button = tk.Button(root, text="Generate password", command=self.generate_password)
+        self.generate_button = tk.Button(self.root, text="Generate password", command=self.generate_password)
         self.generate_button.place(relx=0.5, y=265, width=160, height=30, anchor="center")
     
         # configuring the field for the generated password
-        self.result_label = tk.Label(root, bg="white", padx=10, text="Your password will appear here.")
+        self.result_label = tk.Label(self.root, bg="white", padx=10, text="Your password will appear here.")
         self.result_label.place(relx=0.5, y=340, anchor="center")
 
         # configuring the button for copying the result to the clipboard
-        self.copy_button = tk.Button(root, state=tk.DISABLED, text="Copy password to clipboard", command= lambda : pyperclip.copy(self.result_label.cget("text")))
+        self.copy_button = tk.Button(self.root, state=tk.DISABLED, text="Copy password to clipboard", command= lambda : pyperclip.copy(self.result_label.cget("text")))
         self.copy_button.place(relx=0.5, y=385, width=220, height=30, anchor="center")
 
         # configuring the exit button
-        self.exit_button = tk.Button(root, text="Exit", command=root.destroy)
+        self.exit_button = tk.Button(self.root, text="Exit", command=root.destroy)
         self.exit_button.place(relx=0.9, rely=0.9, anchor="center")
 
     def generate_password(self):
